3d7387dfea32c2487ec268230946a5ca2a3afeb0
mcestuya <93035929+mcestuya@users.noreply.github.com>
2022-03-23 23:07:06 +0000

    Forecast added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from "react";
 import "./App.css";
 import Search from "./Search";
 import WeatherDisplay from "./WeatherDisplay";
+import WeatherForecast from "./WeatherForecast";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather Buddy</h1>
-        <Search />
-        <WeatherDisplay />
-      </header>
+      <div class="container">
+        <div class="weather-app-wrapper">
+          <div class="weather-app">
+            <h1>Weather Buddy</h1>
+            <Search />
+            <WeatherDisplay />
+            <WeatherForecast />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Forecast.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Forecast from "./Forecast";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  return (
+    <div className="container">
+      <div class="weather-forecast" id="forecast">
+        <div class="row">
+          <Forecast />
+          <Forecast />
+          <Forecast />
+          <Forecast />
+          <Forecast />
+          <Forecast />
+        </div>
+      </div>
+    </div>
+  );
+}
